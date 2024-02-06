36b53d23b54df317f15b6a2e99e41dba791a9890
Muharrem Tekin <muharremtekinn@outlook.com.tr>
2024-02-06 08:38:57 +0300

    Add project files.

---
+++ BilgeAdamProject.WebApi/Extensions/ServicesExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace BilgeAdamProject.WebApi.Extensions;
+
+public static class ServicesExtensions
+{
+    public static void ConfigureSerilog(this IServiceCollection services, IConfiguration configuration)
+    {
+        // log conf
+    }
+}
+
---
+++ BilgeAdamProject.WebApi/Middlewares/RequestLoggerMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace BilgeAdamProject.WebApi.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class RequestLoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestLoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class RequestLoggerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestLoggerMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestLoggerMiddleware>();
+        }
+    }
+}
